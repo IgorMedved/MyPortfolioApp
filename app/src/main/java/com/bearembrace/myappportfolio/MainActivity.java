98dc9a665f50e20d2d80ef5cefa07429502feda2
IgorMed <igor.medvedyev@gmail.com>
2016-11-19 22:15:03 -0500

    Code review suggestions implemented: 1) improved code identations and spacing 2) used Button android:onClick xml attributes instead of anonymous classes for processing button clicks 3) moved common button xml attributes into separate style 	<!--style for project button appearance -->     <style name="PrimaryButton" parent="Widget.AppCompat.Button.Colored">         <item name="android:layout_width">match_parent</item>         <item name="android:layout_height">wrap_content</item>         <item name="android:textColor">?android:textColorPrimary</item>         <item name="android:onClick">display</item>     </style>

--- app/src/main/java/com/bearembrace/myappportfolio/MainActivity.java
+++ app/src/main/java/com/bearembrace/myappportfolio/MainActivity.java
@@ -9,95 +9,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnProj1;
-    private Button btnProj2;
-    private Button btnProj3;
-    private Button btnProj4;
-    private Button btnProj5;
-    private Button btnProj6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initialize buttons
-
-        btnProj1=(Button)findViewById(R.id.project1_btn);
-        btnProj2=(Button)findViewById(R.id.project2_btn);
-        btnProj3=(Button)findViewById(R.id.project3_btn);
-        btnProj4=(Button)findViewById(R.id.project4_btn);
-        btnProj5=(Button)findViewById(R.id.project5_btn);
-        btnProj6=(Button)findViewById(R.id.project6_btn);
-
-        // add onClickListener to each button
-        btnProj1.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                // pass corresponding app name to display in toast
-                display(getResources().getString(R.string.project1));
-            }
-        });
-
-
-        btnProj2.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                display(getResources().getString(R.string.project2));
-            }
-        });
-
-        btnProj3.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                display(getResources().getString(R.string.project3));
-            }
-        });
-
-        btnProj4.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                display(getResources().getString(R.string.project4));
-            }
-        });
-
-        btnProj5.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                display(getResources().getString(R.string.project5));
-            }
-        });
-
-        btnProj6.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                display(getResources().getString(R.string.project6));
-            }
-        });
-
-
-
-
-
-
     }
 
-    // displays a toast when a button is pressed
-    public void display (CharSequence text)
+    // displays a toast when a button is pressed (activated from buttons onClick attribute defined in PrimaryButton style
+    public void displayToast (View view)
     {
-        Toast toast = Toast.makeText(getApplicationContext(), "This button will show "+ text + " App" , Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(), "This button will show "+ ((Button)view).getText()+ " App" , Toast.LENGTH_SHORT);
         toast.show();
     }
 }
